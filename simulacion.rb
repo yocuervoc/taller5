413c767eb6b1c5f3fd021d150326fa44596a4286
yocuervoc <yocuervoc@unal.edu.co>
2018-12-14 02:43:44 -0500

    arreglo bug cuando la fila esta vacia

--- simulacion.rb
+++ simulacion.rb
@@ -5,18 +5,40 @@ class Caja
     @fila=fila
   end
 
-  def atender_cliente
+  def atender_cliente(numero_fila)
 
     if fila[0].class==CLiente
-      puts "|C|"
       if !fila.empty?
         cliente=fila.delete_at(0)
+        cliente.tiempo_en_caja=4+rand(21)
       end
     end
 
     if fila[0].class==Array
-      # TODO:
+      if !fila.empty?
+        cliente= fila[numero_fila].delete_at(0)
+        cliente.tiempo_en_caja=4+rand(21)
+      end
     end
+
+  end
+
+  def dibujar_caja
+
+    if fila.empty?
+      print "           |C|             "
+    else
+      if fila[0].class==CLiente
+        print "           |C|             "
+      end
+
+      if fila[0].class==Array
+        fila.each do |i|
+          puts "     |C|"
+        end
+      end
+    end
+
 
   end
 end
@@ -38,10 +60,6 @@ class Fila
     @clientes=[]
    end
 
-   def agregar_cliente_fila(cliente)
-     clientes.push(cliente)
-   end
-
    def dibujar_fila
      # TODO:
    end
@@ -50,21 +68,20 @@ end
 
 class Simulacion
 
-  attr_accessor :caja, :cliente, :numero_de_clientes, :sumatoria_tiempo_espera
+  attr_accessor :caja, :numero_de_clientes, :sumatoria_tiempo_espera
 
-  def initialize(caja, cliente, numero_de_clientes, sumatoria_tiempo_espera)
+  def initialize(caja,  numero_de_clientes, sumatoria_tiempo_espera)
     @caja=caja
-    @cliente=cliente
-    @numero_de_clientes=numero_de_clientes
-    @sumatoria_tiempo_espera=sumatoria_tiempo_espera
+    @numero_de_clientes=0
+    @sumatoria_tiempo_espera=0
   end
 
   def tiempo_espera_caja
-    caja.cliente.tiempo_en_caja= 4+rand(26)
+    caja.cliente.tiempo_en_caja= 4+rand(21)
   end
 
   def encolar_cliente
-    random_name = %w{a b c d e f g h i j k l m n o pq r s t u v w x y z}
+    random_name = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z}
     n=random_name.length
 
     if caja.fila[0].class= Cliente
@@ -84,16 +101,6 @@ class Simulacion
     end
   end
 
-  def desencolar_cliente(numero_fila)
-    if fila[0].class= Cliente
-      fila.delete_at(0)
-    end
-
-    if fila[0].class= Array
-      fila[numero_fila].delete_at(0)
-    end
-
-  end
 end
 
 fila=Fila.new()
@@ -101,36 +108,12 @@ caja=Caja.new(fila)
 cliente=Cliente.new("N")
 cliente2=Cliente.new("N")
 
-class Prueba
-  attr_accessor :dato1, :dato2
 
-  def initialize(dato1)
-    @dato1=dato1
-  end
-end
-
-p=Prueba.new(12)
-
-puts p.dato1
-puts p.dato2
-
-if p.dato2==nil
-  puts "dato 2 = nulo"
-end
-p.dato1=nil
-if p.dato1==nil
-  puts "dato 1 = nulo"
-else
-  puts "dato 1 = NOnulo"
-end
+caja.dibujar_caja
 
 
 
 
 
 
-
-
-
-
-###
+##
