41bb44c27ae77f244c815d58d5ba46188fe06947
Stiven <stiven.maldonado@brm.com.co>
2019-01-28 15:10:47 -0500

    delete spaces in whith

--- simulacion.rb
+++ simulacion.rb
@@ -1,25 +1,19 @@
 class Cliente
   attr_accessor :nombre, :tiempo_espera_fila, :tiempo_en_caja
-
   def initialize(nombre)
     @nombre=nombre
     @tiempo_espera_fila=0
     @tiempo_en_caja=0
   end
 end
-
-
 class Caja
   attr_accessor :fila, :cliente, :sumatoria_tiempo_espera_caja
-
   def initialize(fila, cliente)
     @fila=fila
     @cliente=cliente
     @sumatoria_tiempo_espera_caja = 0
   end
-
   def atender_cliente
-
     if fila.class==Fila
       if @cliente.nombre ==nil
         if !fila.clientes.empty?
@@ -29,39 +23,31 @@ class Caja
         end
       end
     end
-
     if fila.class==Array
       if @cliente.nombre ==nil
         if !fila.empty?
           @sumatoria_tiempo_espera_caja += fila[0].tiempo_espera_fila
           @cliente=fila.delete_at(0)
           @cliente.tiempo_en_caja=4+rand(21)
-
         end
       end
     end
-
   end
-
   def disminuir_tiempo_en_caja
     if @cliente.tiempo_en_caja!=nil
       if @cliente.tiempo_en_caja >0
         @cliente.tiempo_en_caja-=1
       end
-
     end
   end
-
   def abandonar_caja
     if cliente.tiempo_en_caja==0
       @cliente=Cliente.new(nil)
     end
   end
-
   def dibujar_caja
     print "          |C|"
   end
-
   def dibujar_cliente_caja
     if cliente.nombre==nil
       print "          | |"
@@ -70,24 +56,18 @@ class Caja
     end
   end
 end
-
-
 class Fila
    attr_accessor :clientes
-
    def initialize()
     @clientes=[]
    end
-
    def dibujar_fila
-
      if clientes[0].class== Cliente
        #puts "dibuja en clase cliente"
        clientes.each do |i|
          puts "    |#{i.nombre}|"
        end
      end
-
      if clientes[0].class==Array
        #puts "dibuja a clase array"
        fila_mas_larga=1
@@ -96,7 +76,6 @@ class Fila
            fila_mas_larga=i.length
          end
        end
-
        c=0
        contador=1
        for j in 0...fila_mas_larga
@@ -120,7 +99,6 @@ class Fila
        puts "        | |"
      end
    end
-
    def aumenta_tiempo_espera_fila
      clientes.each do |i|
        if i.class== Cliente
@@ -134,11 +112,8 @@ class Fila
      end
    end
 end
-
 class Simulacion
-
   attr_accessor :cajas, :filas, :numero_de_clientes, :sumatoria_tiempo_espera,  :cantidad_de_cajas, :cantidad_de_filas
-
   def initialize(cantidad_de_cajas, cantidad_de_filas)
     @cajas=[]
     @filas=Fila.new()
@@ -146,16 +121,12 @@ class Simulacion
     @sumatoria_tiempo_espera=0
     @cantidad_de_cajas=cantidad_de_cajas
     @cantidad_de_filas=cantidad_de_filas
-
-
     if cantidad_de_filas > 1
       cantidad_de_filas.times do
         filas.clientes.push([])
       end
     end
-
     for i in (0..cantidad_de_cajas)
-
       if cantidad_de_filas==1
         for i in (0...cantidad_de_cajas)
           cliente=Cliente.new(nil)
@@ -169,16 +140,12 @@ class Simulacion
       end
     end
   end
-
   def encolar_cliente
     random_name = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z}
     n=random_name.length
-
-
     if filas.clientes[0].class== Cliente
       filas.clientes.push(Cliente.new(random_name[rand(n)]))
     end
-
     if filas.clientes[0].class==Array
       fila_mas_corta =filas.clientes[0].length
       contador=0
@@ -196,33 +163,25 @@ class Simulacion
         end
         contador+=1
       end
-
       filas.clientes[posicion].push(Cliente.new(random_name[rand(n)]))
     end
-
     if filas.clientes[0].class== NilClass
       filas.clientes.push(Cliente.new(random_name[rand(n)]))
     end
   end
-
 end
-
 class App
-
   puts "Tipos de simulacion:"
   puts "* Unica fila: oprima 1"
   puts "* multiples filas: oprima 2"
   cantidad_de_filas=gets.to_i
   puts "ingrese la cantidad de cajas"
   cantidad_de_cajas=gets.to_i
-
   if cantidad_de_filas==1
     simulacion=Simulacion.new(cantidad_de_cajas,1)
   else
     simulacion=Simulacion.new(cantidad_de_cajas,cantidad_de_cajas)
-
   end
-
   puts "ingrese le tiempo de simulacion"
   tiempo_de_simulacion=gets.to_i
   puts "ingrese el delta"
@@ -232,7 +191,6 @@ class App
   while tiempo_reloj<=tiempo_de_simulacion
     puts "  Minuto #{tiempo_reloj}"
     puts "________________________________________________________"
-
     if tiempo_reloj%3 == 0
       clientes_nuevos=rand(5)
       puts "encola #{clientes_nuevos} clientes"
@@ -241,8 +199,6 @@ class App
         simulacion.encolar_cliente
       end
     end
-
-
     simulacion.filas.aumenta_tiempo_espera_fila
     c=0
     simulacion.cajas.each do |i|
@@ -270,28 +226,20 @@ class App
       simulacion.cajas[c].abandonar_caja
       c+=1
     end
-
     puts ""
     puts "       ------------------------------------------------"
-
     simulacion.filas.dibujar_fila
-
     sleep(delta)
     tiempo_reloj+=1
     puts "________________________________________________________"
   end
-
   c=0
   simulacion.cajas.each do |i|
     simulacion.sumatoria_tiempo_espera += simulacion.cajas[c].sumatoria_tiempo_espera_caja
     c+=1
   end
-
 puts "tiemp total de espera de todos lo clientes atendidos fue de #{simulacion.sumatoria_tiempo_espera}"
 puts "tiempo promedio de espera de los clientes atndidos fue de: #{simulacion.sumatoria_tiempo_espera/simulacion.numero_de_clientes} minutos"
 end
-
 App.new()
-
 #by Yocc
-
